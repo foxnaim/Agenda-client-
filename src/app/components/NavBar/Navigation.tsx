234aa3114bdb4a209ed42bc3709124afadf44e23
Rick <118506963+foxnaim@users.noreply.github.com>
2025-02-20 02:13:15 +0500

    modified:   src/app/components/NavBar/Navigation.tsx

--- src/app/components/NavBar/Navigation.tsx
+++ src/app/components/NavBar/Navigation.tsx
@@ -7,26 +7,26 @@ const Navigation = () => {
   return (
     <nav className="fixed left-0 top-1/2 -translate-y-1/2 flex flex-col justify-between items-center p-4 bg-dop w-[60px] h-[350px] rounded-r-3xl hover:w-[160px] duration-300">
       <ul className="text-white flex flex-col justify-around h-full w-full">
-        <li className="group flex items-center gap-3">
-          <TiHome className="w-7 h-7" />
+        <li className="flex items-center gap-3 group">
+          <TiHome className="w-7 h-7 text-white" /> 
           <span className="opacity-0 group-hover:opacity-100 duration-300 text-lg whitespace-nowrap">
             Home
           </span>
         </li>
-        <li className="group flex items-center gap-3">
-          <FaTasks className="w-7 h-7" />
+        <li className="flex items-center gap-3 group">
+          <FaTasks className="w-7 h-7 text-white" />
           <span className="opacity-0 group-hover:opacity-100 duration-300 text-lg whitespace-nowrap">
             Задачи
           </span>
         </li>
-        <li className="group flex items-center gap-3">
-          <FaUserFriends className="w-7 h-7" />
+        <li className="flex items-center gap-3 group">
+          <FaUserFriends className="w-7 h-7 text-white" />
           <span className="opacity-0 group-hover:opacity-100 duration-300 text-lg whitespace-nowrap">
             Друзья
           </span>
         </li>
-        <li className="group flex items-center gap-3">
-          <CgProfile className="w-7 h-7" />
+        <li className="flex items-center gap-3 group">
+          <CgProfile className="w-7 h-7 text-white" />
           <span className="opacity-0 group-hover:opacity-100 duration-300 text-lg whitespace-nowrap">
             Профиль
           </span>
