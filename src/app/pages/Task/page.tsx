de440c611450821c408040dde7403927afe1c688
Rick <118506963+foxnaim@users.noreply.github.com>
2025-02-24 16:02:17 +0500

    modified:   src/app/pages/Task/page.tsx

--- src/app/pages/Task/page.tsx
+++ src/app/pages/Task/page.tsx
@@ -2,19 +2,15 @@
 import React, { useState } from "react";
 import { Plus } from "lucide-react";
 import Navigation from "@/app/components/SideBar/Navigation";
+import Link from "next/link";
 
 interface Task {
   id: number;
   title: string;
-  description: string;
-  date: string;
 }
 
 export default function AgendaTask() {
-  const [tasks, setTasks] = useState<Task[]>([
-    { id: 1, title: "Agenda Ai", description: "Стартап", date: "15.02.2025" },
-    { id: 2, title: "Mobile dev", description: "Обучение", date: "15.12.2024" },
-  ]);
+  const [tasks, setTasks] = useState<Task[]>([{ id: 1, title: "Agenda Ai" }]);
 
   return (
     <div className="flex h-screen bg-bgop">
@@ -24,18 +20,17 @@ export default function AgendaTask() {
 
       {/* Центрирование и адаптив */}
       <div className="flex-1 flex items-center justify-center p-6">
-        <div className="flex grid-cols-auto-fit gap-6">
+        <div className="flex grid-cols-auto-fit gap-6  ">
           {tasks.map((task) => (
-            <div
+            <Link
+              href="/gib"
               key={task.id}
-              className="w-40 h-40 bg-[#897F68] text-white p-4 rounded-xl flex flex-col justify-between shadow-md"
+              className="w-40 h-40 bg-dop hover:bg-dopHover cursor-pointer duration-300 text-white p-4 rounded-xl flex flex-col justify-between shadow-md"
             >
-              <div>
+              <div className="flex-1 flex flex-col justify-center items-center text-center h-full">
                 <h2 className="font-bold">{task.title}</h2>
-                <p className="text-sm text-gray-200">{task.description}</p>
               </div>
-              <p className="text-sm font-semibold">{task.date}</p>
-            </div>
+            </Link>
           ))}
           <button className="w-40 h-40 border-2 border-[#897F68] rounded-xl flex items-center justify-center hover:bg-[#9C92781A] transition">
             <Plus size={40} color="#897F68" />
