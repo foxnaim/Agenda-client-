3c960e2580975dc495231893f54b2af672b1be2f
Rick <118506963+foxnaim@users.noreply.github.com>
2025-02-26 11:54:13 +0500

    modified:   src/app/components/tasks/TaskMenu/page.tsx

--- src/app/components/tasks/TaskMenu/page.tsx
+++ src/app/components/tasks/TaskMenu/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { Plus, Trash2, Edit } from "lucide-react";
 import { useRouter } from "next/navigation"; 
+
 import AddTask from "@/app/components/tasks/AddTask";
 
 interface Task {
@@ -75,8 +76,8 @@ export default function AgendaTask() {
 
   return (
     <div className=" bg-bgop">
-      <div className="flex-1 flex items-center justify-center p-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="flex-1 flex  items-center justify-center p-6">
+        <div className=" grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {tasks.map((task) => (
             <div
               key={task.id}
