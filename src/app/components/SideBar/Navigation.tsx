5317b0c13a5fe55e0f709bbc5cd743832cdba86c
Rick <118506963+foxnaim@users.noreply.github.com>
2025-02-28 23:03:21 +0500

    modified:   src/app/components/SideBar/Navigation.tsx

--- src/app/components/SideBar/Navigation.tsx
+++ src/app/components/SideBar/Navigation.tsx
@@ -5,32 +5,21 @@ import { CgProfile } from "react-icons/cg";
 
 const Navigation = () => {
   return (
-    <nav className="fixed left-2 top-1/2 -translate-y-1/2 flex flex-col justify-between items-center p-4 bg-dop w-[60px] h-[350px] rounded-r-3xl hover:w-[160px] duration-300">
-      <ul className="text-white flex flex-col justify-around h-full w-full">
-        <li className="hover:flex items-center gap-3 ">
-          <TiHome className="size-7 text-white" /> 
-          <p className="duration-300 opacity-0 hover:opacity-100 text-lg whitespace-nowrap">
-            Home
-          </p>
-        </li>
-        <li className="flex items-center gap-3">
-          <FaTasks className="w-7 h-7 text-white" />
-          <span className="opacity-0 hover:opacity-100 duration-300 text-lg whitespace-nowrap">
-            Задачи
-          </span>
-        </li>
-        <li className="flex items-center gap-3">
-          <FaUserFriends className="w-7 h-7 text-white" />
-          <span className="opacity-0 hover:opacity-100 duration-300 text-lg whitespace-nowrap">
-            Друзья
-          </span>
-        </li>
-        <li className="flex items-center gap-3">
-          <CgProfile className="w-7 h-7 text-white" />
-          <span className="opacity-0 hover:opacity-100 duration-300 text-lg whitespace-nowrap">
-            Профиль
-          </span>
-        </li>
+    <nav className="fixed left-2 top-1/2 -translate-y-1/2 flex flex-col items-center p-4 bg-dop w-[90px] h-[350px] rounded-r-3xl">
+      <ul className="text-white flex flex-col gap-4 w-full">
+        {[
+          { icon: <TiHome className="size-[50px]" />, text: "Home" },
+          { icon: <FaTasks className="size-[50px]" />, text: "Задачи" },
+          { icon: <FaUserFriends className="size-[50px]" />, text: "Друзья" },
+          { icon: <CgProfile className="size-[50px]" />, text: "Профиль" },
+        ].map((item, index) => (
+          <li key={index} className="relative flex items-center gap-3 p-2 hover:bg-gray-700 rounded-lg cursor-pointer group">
+            {item.icon}
+            <span className="absolute left-12 opacity-0 group-hover:opacity-100 bg-gray-900 text-white text-sm px-3 py-1 rounded-md shadow-md transition-opacity duration-300">
+              {item.text}
+            </span>
+          </li>
+        ))}
       </ul>
     </nav>
   );
