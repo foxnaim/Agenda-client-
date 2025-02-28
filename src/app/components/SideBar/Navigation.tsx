0c0b615bea1958cfa82137bf2436007d33062232
Rick <118506963+foxnaim@users.noreply.github.com>
2025-03-01 01:27:13 +0500

    modified:   src/app/components/SideBar/Navigation.tsx

--- src/app/components/SideBar/Navigation.tsx
+++ src/app/components/SideBar/Navigation.tsx
@@ -6,12 +6,12 @@ import { CgProfile } from "react-icons/cg";
 const Navigation = () => {
   return (
     <nav className="fixed left-2 top-1/2 -translate-y-1/2 flex flex-col items-center p-4 bg-dop w-[90px] h-[350px] rounded-3xl">
-      <ul className="text-white flex flex-col gap-6 w-full">
+      <ul className="text-white flex flex-col gap-8 w-full">
         {[
-          { icon: <TiHome className="size-9" />, text: "Home" },
-          { icon: <FaTasks className="size-9" />, text: "Задачи" },
-          { icon: <FaUserFriends className="size-9" />, text: "Друзья" },
-          { icon: <CgProfile className="size-9" />, text: "Профиль" },
+          { icon: <TiHome className="size-8" />, text: "Home" },
+          { icon: <FaTasks className="size-8" />, text: "Задачи" },
+          { icon: <FaUserFriends className="size-8" />, text: "Друзья" },
+          { icon: <CgProfile className="size-8" />, text: "Профиль" },
         ].map((item, index) => (
           <li key={index} className="relative flex items-center gap-3 p-2 hover:bg-dopHover rounded-2xl cursor-pointer group">
             {item.icon}
