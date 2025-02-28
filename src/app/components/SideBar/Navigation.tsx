737ee95a6551ef5009b08f98a63b10ecb231f0b6
Rick <118506963+foxnaim@users.noreply.github.com>
2025-03-01 02:19:45 +0500

    new file:   src/app/components/GetOut/index.tsx 	deleted:    src/app/components/GetOut/page.tsx 	modified:   src/app/components/SideBar/Navigation.tsx 	modified:   src/app/pages/Profile/page.tsx

--- src/app/components/SideBar/Navigation.tsx
+++ src/app/components/SideBar/Navigation.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { TiHome } from "react-icons/ti";
 import { FaTasks, FaUserFriends } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 
 const Navigation = () => {
+  const [isProfileMenuOpen, setProfileMenuOpen] = useState(false);
+
   return (
     <nav className="fixed left-2 top-1/2 -translate-y-1/2 flex flex-col items-center p-4 bg-dop w-[90px] h-[350px] rounded-3xl">
       <ul className="text-white flex flex-col gap-8 w-full">
@@ -11,7 +14,6 @@ const Navigation = () => {
           { icon: <TiHome className="size-8" />, text: "Home" },
           { icon: <FaTasks className="size-8" />, text: "Задачи" },
           { icon: <FaUserFriends className="size-8" />, text: "Друзья" },
-          { icon: <CgProfile className="size-8" />, text: "Профиль" },
         ].map((item, index) => (
           <li key={index} className="relative flex items-center gap-3 p-2 hover:bg-dopHover rounded-2xl cursor-pointer group">
             {item.icon}
@@ -20,6 +22,25 @@ const Navigation = () => {
             </span>
           </li>
         ))}
+
+        {/* Профиль с выпадающим меню */}
+        <li className="relative flex items-center gap-3 p-2 hover:bg-dopHover rounded-2xl cursor-pointer group">
+          <button onClick={() => setProfileMenuOpen(!isProfileMenuOpen)}>
+            <CgProfile className="size-8" />
+          </button>
+          <span className="absolute left-14 opacity-0 group-hover:opacity-100 bg-dop text-white text-sm px-3 py-1 rounded-lg shadow-md transition-opacity duration-300">
+            Профиль
+          </span>
+
+          {/* Выпадающее меню */}
+          {isProfileMenuOpen && (
+            <ul className="absolute left-14 top-10 bg-dop text-white text-sm rounded-lg shadow-md py-2 w-32">
+              <li className="p-2 hover:bg-dopHover cursor-pointer">Профиль</li>
+              <li className="p-2 hover:bg-dopHover cursor-pointer">Настройки</li>
+              <li className="p-2 hover:bg-red-500 cursor-pointer">Выйти</li>
+            </ul>
+          )}
+        </li>
       </ul>
     </nav>
   );
--- src/app/pages/Profile/page.tsx
+++ src/app/pages/Profile/page.tsx
@@ -1,10 +1,13 @@
+import Navigation from '@/app/components/SideBar/Navigation'
 import React from 'react'
 
 type Props = {}
 
 const Profile = (props: Props) => {
   return (
-    <div>page</div>
+   <React.Fragment>
+    <Navigation/>
+   </React.Fragment>
   )
 }
 
