e6ac0269b8182065edcec1e4132df63fb2988278
Rick <118506963+foxnaim@users.noreply.github.com>
2025-02-28 22:16:18 +0500

    modified:   src/app/pages/TaskDashboard/page.tsx

--- src/app/pages/TaskDashboard/page.tsx
+++ src/app/pages/TaskDashboard/page.tsx
@@ -1,5 +1,4 @@
 import MonthsDashboard from '@/app/components/Dashboard/Dashboard12'
-import DashboardTask from '@/app/components/Dashboard/DashboardTask'
 import Navigation from '@/app/components/SideBar/Navigation'
 import AgendaTask from '@/app/components/tasks/TaskMenu/page'
 import Time from '@/app/components/Time'
@@ -12,7 +11,7 @@ const TaskDashboard  = () => {
    <React.Fragment>
     <Navigation/>
     <div className='flex flex-row justify-between items-center '>
-     <div className='block ml-[200px]'>
+     <div className='block ml-[250px] mt-5'>
     <MonthsDashboard/>
      </div>
      <div className='block mr-3'>
