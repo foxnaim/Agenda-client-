978908cc11c6136ac17b1cd05b2ecae34313749b
Rick <118506963+foxnaim@users.noreply.github.com>
2025-02-28 21:43:10 +0500

    modified:   src/app/components/Time/index.tsx

--- src/app/components/Time/index.tsx
+++ src/app/components/Time/index.tsx
@@ -11,24 +11,19 @@ export default function Time() {
 
   const hours = time.getHours();
   const minutes = time.getMinutes();
-  const seconds = time.getSeconds();
   const ampm = hours >= 12 ? "PM" : "AM";
   const day = time.toLocaleDateString("ru-RU", { weekday: "long" });
 
-
-
   return (
-    <div className="flex flex-col  items-center p-4 bg-dop text-white rounded-2xl shadow-lg  w-[300px] h-[100px]">
-      {/* Цифровые часы */}
-      <div className="text-4xl font-bold flex gap-2">
-        <span>{String(hours).padStart(2, "0")}:</span>
-        <span>{String(minutes).padStart(2, "0")}:</span>
-        <span>{String(seconds).padStart(2, "0")}</span>
-        <span className="text-xl">{ampm}</span>
+    <div className="flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center p-4 bg-dop text-white rounded-2xl shadow-lg w-[160px] h-[100px]">
+        <div className="text-4xl font-bold flex gap-2">
+          <span>{String(hours).padStart(2, "0")}:</span>
+          <span>{String(minutes).padStart(2, "0")}</span>
+          <span className="text-xl">{ampm}</span>
+        </div>
+        <div className="text-lg text-gray-400 capitalize">{day}</div>
       </div>
-      <div className="text-lg text-gray-400 capitalize">{day}</div>
-
-      {/* Аналоговые часы */}
     </div>
   );
 }
