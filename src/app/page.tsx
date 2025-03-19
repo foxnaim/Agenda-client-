f62c6d2c20179f94c958f539b9fe35eeaaec2394
Alohadauka <dauletorazalov@gmail.com>
2025-03-19 18:48:30 +0000

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
-import Chat from './pages/Chat/page'
 import Login from './pages/login/[id]/page'
-=======
-import Login from './pages/login/[id]/page'
-
-
->>>>>>> d95ca5666898b383e401631e891b85f33e8b212c
 const page = () => {
   return (
     <React.Fragment>
